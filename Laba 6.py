b4407eecb0c2f4c9da2604559fd6ad4ad049b4f3
Pavel <pavel.img@gmail.com>
2024-09-18 20:06:19 +0200

    Лабораторные работы 1,3,4,5,6

--- Laba 6.py
+++ Laba 6.py
@@ -1,32 +1,28 @@
 from random import randint
 
 # Случайный список из 10 элементов от 1 до 99
-a = [randint(1, 99) for i in range(4)]
-print(a)
+a = [randint(1, 99) for i in range(10)]
 
 
 def selection_sort_simple_1(arr):
     for i in range(len(arr) - 1, 0, -1):
         max_element_index = arr.index(max(arr[:i + 1]))
         arr[i], arr[max_element_index] = arr[max_element_index], arr[i]
-        return arr
-
-
-def selection_sort_simple_2(arr):
-    for i in range(len(arr) - 1, 0, -1):
-        max_element_index = -1
-        max_element = 0
-        for index, element in enumerate(arr):
-            if element >= max_element:
-                max_element = element
-                max_element_index = index
-        arr[i], arr[max_element_index] = arr[max_element_index], arr[i]
     return arr
 
 
-def selection_sort_double(arr):
-    pass
+def selection_sort_simple_2(arr):
+    i = 0
+    while i < len(arr) - 1:
+        m = i
+        j = i + 1
+        while j < len(arr):
+            if arr[j] < arr[m]:
+                m = j
+            j += 1
+        arr[i], arr[m] = arr[m], arr[i]
+        i += 1
+    return arr
 
 
-print(selection_sort_simple_1(a), selection_sort_simple_2(a), selection_sort_double(a), sep="\n")
-# https://habr.com/ru/articles/422085/
+print(a, selection_sort_simple_1(a.copy()), selection_sort_simple_2(a.copy()), sep="\n")
